065c9b8badb93cb7fb5381488a8f141de5b39bca
Aaron Passchier <ap007@aquinas.edu>
2020-12-25 23:22:42 -0500

    Implemented Query Services

--- DynamicQuerying/DynamicQuerying.Main/Query/Services/QueryService.cs
+++ DynamicQuerying/DynamicQuerying.Main/Query/Services/QueryService.cs
@@ -1,7 +1,38 @@
-﻿namespace DynamicQuerying.Main.Query.Services
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DynamicQuerying.Main.Expressions;
+using DynamicQuerying.Main.Extensions;
+using DynamicQuerying.Main.Query.Models;
+
+namespace DynamicQuerying.Main.Query.Services
 {
     public class QueryService
     {
-        
+        public static QueryResponse<T> GetQueryResponse<T>(IQueryable<T> source, QueryRequest queryRequest)
+        {
+            var filtered = source.FilterBy(queryRequest.Filters);
+            var ordered = filtered.OrderBy(queryRequest.Sorters);
+            var response = QueryExpressions.PaginateRequest(ordered, queryRequest);
+            FinishResponse(filtered, queryRequest, response);
+            return response;
+        }
+
+        public static Task<QueryResponse<T>> GetQueryResponseAsync<T>(IQueryable<T> source, QueryRequest queryRequest)
+        {
+            return Task.Run(() => GetQueryResponse(source, queryRequest));
+        }
+
+        public static void FinishResponse<T>(
+            IQueryable<T> source,
+            QueryRequest queryRequest,
+            QueryResponse<T> queryResponse)
+        {
+            var filtered = source.FilterBy(queryRequest.Filters);
+            var _ = filtered
+                .AsEnumerable()
+                .GroupBy(item => 1)
+                .Select(grouping => QueryExpressions.FinishRequest(grouping, queryRequest, queryResponse))
+                .ToList();
+        }
     }
 }
