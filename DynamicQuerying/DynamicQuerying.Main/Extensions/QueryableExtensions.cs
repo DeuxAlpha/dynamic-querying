6cc1b3fc60e25490d25de2e54393c8145a66ce3d
Aaron Passchier <ap007@aquinas.edu>
2020-12-25 23:22:34 -0500

    Added queryable extensions

---
+++ DynamicQuerying/DynamicQuerying.Main/Extensions/QueryableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DynamicQuerying.Main.Expressions;
+using DynamicQuerying.Main.Query.Filtering;
+using DynamicQuerying.Main.Query.Sorting;
+
+namespace DynamicQuerying.Main.Extensions
+{
+    internal static class QueryableExtensions
+    {
+        public static IQueryable<T> FilterBy<T>(this IQueryable<T> source, IEnumerable<Filter> filters)
+        {
+            return FilterExpressions.FilterByMembers(source, filters);
+        }
+
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, IEnumerable<Sorter> sorters)
+        {
+            if (sorters == null) return source;
+            var sorterList = sorters.ToList();
+            if (sorterList.Empty()) return source;
+            for (var index = 0; index < sorterList.Count; index++)
+            {
+                var sorter = sorterList[index];
+                source = index == 0
+                    ? SorterExpressions.OrderByMember(source, sorter)
+                    : SorterExpressions.ThenOrderByMember(source, sorter);
+            }
+
+            return source;
+        }
+    }
+}
