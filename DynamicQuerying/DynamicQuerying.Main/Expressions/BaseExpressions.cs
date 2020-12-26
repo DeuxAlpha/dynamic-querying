4028b941dd0a51f12878fb4e47c51c65d6559103
Aaron Passchier <ap007@aquinas.edu>
2020-12-25 22:28:13 -0500

    Added base expressions

---
+++ DynamicQuerying/DynamicQuerying.Main/Expressions/BaseExpressions.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DynamicQuerying.Main.Expressions
+{
+    internal static class BaseExpressions
+    {
+        // TODO: Considers migrating expressions to extensions...?
+        public static Expression GetDotMember(Expression expression, string propertyName)
+        {
+            return propertyName.Split('.').Aggregate(expression, Expression.PropertyOrField);
+        }
+
+        public static Expression ConvertValue(Expression property, object value)
+        {
+            var propertyType = property.Type;
+            var constant = Expression
+                .Constant(TypeDescriptor.GetConverter(propertyType)
+                    .ConvertFromInvariantString(value.ToString()));
+            var converted = Expression.Convert(constant, propertyType);
+            return converted;
+        }
+    }
+}
