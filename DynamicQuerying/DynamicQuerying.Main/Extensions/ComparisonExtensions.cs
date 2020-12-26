e0e0d8532aa1e14d27f424c1626be5296cff9594
Aaron Passchier <ap007@aquinas.edu>
2020-12-25 22:22:21 -0500

    Added some simple extension classes

---
+++ DynamicQuerying/DynamicQuerying.Main/Extensions/ComparisonExtensions.cs
@@ -0,0 +1,14 @@
+﻿using DynamicQuerying.Main.Query.Filtering.Enums;
+
+namespace DynamicQuerying.Main.Extensions
+{
+    internal static class ComparisonExtensions
+    {
+        public static bool IsStringComparison(this Comparison comparison)
+        {
+            return comparison == Comparison.Contains ||
+                   comparison == Comparison.StartsWith ||
+                   comparison == Comparison.EndsWith;
+        }
+    }
+}
---
+++ DynamicQuerying/DynamicQuerying.Main/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamicQuerying.Main.Extensions
+{
+    internal static class EnumerableExtensions
+    {
+        public static bool Empty<T>(this IEnumerable<T> source)
+        {
+            return !source.Any();
+        }
+    }
+}
