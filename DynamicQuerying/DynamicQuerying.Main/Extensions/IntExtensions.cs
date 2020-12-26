02db4cf344ce9d3ad2e84b2a20c4f96ac13fd873
Aaron Passchier <ap007@aquinas.edu>
2020-12-25 23:15:55 -0500

    Added int extensions

---
+++ DynamicQuerying/DynamicQuerying.Main/Extensions/IntExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DynamicQuerying.Main.Extensions
+{
+    public static class IntExtensions
+    {
+        public static int ThisIfLess(this int number, int comparison)
+        {
+            return number < comparison ? comparison : number;
+        }
+    }
+}
