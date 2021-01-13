b4be52d2430f49dfbb0134211451a656a98ef03a
Aaron Passchier <ap007@aquinas.edu>
2021-01-13 08:57:22 -0500

    Added another request model

---
+++ DynamicQuerying/DynamicQuerying.Sample/Communication/Requests/XlsxImportRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicQuerying.Sample.Communication.Requests
+{
+    public class XlsxImportRequest
+    {
+        public IFormFile ExcelFile { get; set; }
+        public string SheetName { get; set; }
+    }
+}
