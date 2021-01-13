e9ac93215ff0844b9eeb9bda27aba8f2f730f7b4
Aaron Passchier <ap007@aquinas.edu>
2021-01-13 09:30:17 -0500

    Added member map extensions to easily retrieve names

---
+++ DynamicQuerying/DynamicQuerying.Sample/Extensions/MemberMapExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using CsvHelper.Configuration;
+
+namespace DynamicQuerying.Sample.Extensions
+{
+    public static class MemberMapExtensions
+    {
+        public static string GetAssignedName(this MemberMap memberMap)
+        {
+            var assignedName = memberMap.Data.Names.FirstOrDefault();
+            return assignedName ?? memberMap.Data.Member.Name;
+        }
+
+        public static string GetOriginalName(this MemberMap memberMap)
+        {
+            return memberMap.Data.Member.Name;
+        }
+    }
+}
