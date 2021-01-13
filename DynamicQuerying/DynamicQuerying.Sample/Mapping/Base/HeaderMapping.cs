7254b6f9e81b53382e9466b30717b26c9f8dfa69
Aaron Passchier <ap007@aquinas.edu>
2021-01-13 08:56:38 -0500

    Added useless header mapping class

---
+++ DynamicQuerying/DynamicQuerying.Sample/Mapping/Base/HeaderMapping.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CsvHelper.Configuration;
+
+namespace DynamicQuerying.Sample.Mapping.Base
+{
+    // Only inherits for better understanding of purpose.
+    public abstract class HeaderMapping<T> : ClassMap<T>
+    {
+    }
+}
