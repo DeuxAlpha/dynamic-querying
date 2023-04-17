82b0bded378f27e9cf4460ea0e96a3ed762033c8
Aaron Parsons <Aaron.Parsons@bissell.com>
2023-04-17 15:56:10 -0400

    Updated import and export service

--- DynamicQuerying/DynamicQuerying.Sample/Services/ExportService.cs
+++ DynamicQuerying/DynamicQuerying.Sample/Services/ExportService.cs
@@ -5,31 +5,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using ClosedXML.Excel;
 using CsvHelper;
+using CsvHelper.Configuration;
 using DynamicQuerying.Sample.Extensions;
-using DynamicQuerying.Sample.Mapping.Base;
 using Newtonsoft.Json;
 
 namespace DynamicQuerying.Sample.Services
 {
     public static class ExportService
     {
-        public static async Task<byte[]> ExportDataToXlsx<T>(IEnumerable<T> data, HeaderMapping<T> mapping)
+        public static async Task<byte[]> ExportDataToXlsx<T>(IEnumerable<T> data)
         {
             using var workbook = new XLWorkbook();
             var worksheet = workbook.AddWorksheet();
             var headerRow = worksheet.FirstRow();
-            for (var headerIndex = 0; headerIndex < mapping.MemberMaps.Count; headerIndex++)
+            var properties = typeof(T).GetProperties();
+            for (var headerIndex = 0; headerIndex < properties.Length; headerIndex++)
             {
-                headerRow.Cell(headerIndex + 1).SetValue(mapping.MemberMaps[headerIndex].GetAssignedName());
+                headerRow.Cell(headerIndex + 1).SetValue(properties[headerIndex].Name);
             }
 
             foreach (var row in data)
             {
                 var nextRow = worksheet.LastRowUsed().RowBelow();
-                var properties = row.GetType().GetProperties();
                 for (var columnIndex = 0; columnIndex < properties.Length; columnIndex++)
                 {
-                    nextRow.Cell(columnIndex + 1).SetValue(properties[columnIndex].GetValue(row, null));
+                    var cellValue = new XLCellValue();
+                    cellValue.AssignValue(properties[columnIndex].Name, properties[columnIndex].GetValue(row, null));
+                    nextRow.Cell(columnIndex + 1).SetValue(cellValue);
                 }
             }
 
@@ -38,18 +40,14 @@ namespace DynamicQuerying.Sample.Services
             return memoryStream.ToArray();
         }
 
-        public static async Task<byte[]> ExportDataToCsv<T>(IEnumerable<T> data, HeaderMapping<T> mapping, string delimiter)
+        public static async Task<byte[]> ExportDataToCsv<T>(IEnumerable<T> data, string delimiter)
         {
             await using var memoryStream = new MemoryStream();
             await using var streamWriter = new StreamWriter(memoryStream);
-            await using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture)
-            {
-                Configuration =
-                {
-                    Delimiter = delimiter
-                }
-            };
-            csvWriter.Configuration.RegisterClassMap(mapping);
+            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+            csvConfig.Delimiter = delimiter;
+            await using var csvWriter = new CsvWriter(streamWriter, csvConfig);
+            csvWriter.WriteHeader<T>();
             await csvWriter.WriteRecordsAsync(data);
 
             return memoryStream.ToArray();
@@ -57,18 +55,13 @@ namespace DynamicQuerying.Sample.Services
 
         public static async Task<byte[]> ExportDataToJson<T>(
             IEnumerable<T> data,
-            HeaderMapping<T> mapping,
             bool indented)
         {
             await using var memoryStream = new MemoryStream();
             await using var streamWriter = new StreamWriter(memoryStream);
             var serialized = JsonConvert.SerializeObject(
                 data,
-                indented ? Formatting.Indented : Formatting.None,
-                new JsonSerializerSettings
-                {
-                    ContractResolver = new JsonHeaderContractResolver<T>(mapping)
-                });
+                indented ? Formatting.Indented : Formatting.None);
 
             await streamWriter.WriteAsync(serialized);
             await streamWriter.FlushAsync();
